132b71386f40ab4741458c2898adfefadc4529bc
Ganaga <ganaga@laposte.net>
2023-06-23 15:16:22 +0200

    put back express for app engine compatibility

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
 
 import { POI } from "@galigeo-store/shared-models/dist/db";
+import express = require('express');
+
+const port = Number(process.env.PORT) || 5000;
+
+const app = express();
+app.enable('trust proxy');
+
+console.log('Starting server on port ' + port);
 
 console.log('POI comes from artifact registry');
 const poi = new POI();
+
+const server = app.use('/', (req, res, next) => {
+    res.status(200).send('Alive test');
+}).listen(port);
 
