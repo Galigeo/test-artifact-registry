2b0667a9dfd6c58f6bd69e7c7349c4d2a509f626
rtaggo <rteina@galigeo.com>
2023-06-26 10:50:17 +0200

    it works

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
-
 import { POI } from "@galigeo-store/shared-models/dist/db";
-import express = require('express');
-
+import express = require("express");
+import { checkAuth } from "@galigeo-store/auth-lib";
 const port = Number(process.env.PORT) || 5000;
 
 const app = express();
-app.enable('trust proxy');
+app.enable("trust proxy");
 
-console.log('Starting server on port ' + port);
+console.log("Starting server on port " + port);
 
-console.log('POI comes from artifact registry');
+console.log("POI comes from artifact registry");
 const poi = new POI();
 
-const server = app.use('/', (req, res, next) => {
-    res.status(200).send('Alive test');
-}).listen(port);
-
+const server = app
+  .use("/", checkAuth, (req, res, next) => {
+    res.status(200).send("Alive test");
+  })
+  .listen(port);
